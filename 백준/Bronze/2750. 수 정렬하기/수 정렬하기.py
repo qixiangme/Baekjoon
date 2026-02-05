25f9d758a3dba73548d70acc234309844bccc415
erang903 <erang903@khu.ac.kr>
2026-02-05 23:30:25 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=[]
+for i in range(n):arr.append(int(input()))
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i]>arr[j]:
+            tmp=arr[i]
+            arr[i]=arr[j]
+            arr[j]=tmp
+print(*arr,sep="\n")
